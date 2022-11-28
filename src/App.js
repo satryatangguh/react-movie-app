1413c6d3d9a27b6c0377e939343ce39d041330c8
Satrya Tangguh <satryatp@gmail.com>
2022-11-28 13:35:15 +0700

    Add search movie features

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import { Navbar, Container, Nav, Form, FormControl, Button } from 'react-bootstr
 
 
 const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=21ffcbecb3d48c284c502cdad4c17183"
+const API_SEARCH="https://api.themoviedb.org/3/search/movie?api_key=21ffcbecb3d48c284c502cdad4c17183&query"
 function App() {
   
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies]=useState([]);
+  const [query, setQuery]=useState('');
 
   useEffect(() => {
     fetch(API_URL)
@@ -19,31 +21,63 @@ function App() {
     })
   }, [])
   
+  const searchMovie = async(e)=>{
+    e.preventDefault();
+    console.log("Searching");
+    try {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=21ffcbecb3d48c284c502cdad4c17183&query=${query}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setMovies(data.results);
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+
+  const changeHandler=(e)=>{
+    setQuery(e.target.value);
+  }
 
   return (
     <>
       <Navbar bg="dark" expand="lg" variant="dark">
         <Container fluid>
-          <Navbar.Brand href="">Vidport</Navbar.Brand>
-          <Navbar.Brand href="">Trending</Navbar.Brand>
-          <Navbar.Toggle aria-control="navbarScroll"></Navbar.Toggle>
+          <Navbar.Brand href="/home">Vidport</Navbar.Brand>
+          <Navbar.Brand href="/home">Trending</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
           <Navbar.Collapse id="navbarScroll">
             <Nav 
               className="me-auto my-2 my-lg-3" 
               style={{maxHeight:'100px'}}navbarScroll>
             </Nav>
-            <Form className="d-flex">
-              <FormControl type="search" placeholder="Movie Search" className="me-2" aria-label="search" name=""></FormControl>
+            <Form className="d-flex" onSubmit={searchMovie}>
+              <FormControl 
+                type="search" 
+                placeholder="Movie Search" 
+                className="me-2" 
+                aria-label="search" 
+                name="query"
+                value={query}
+                onChange={changeHandler}>
+              </FormControl>
               <Button variant="secondary" type="submit">Search</Button>
             </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className='container'>
-        <div className='grid'>
-          { movies.map((movieReq)=>
-          <MovieBox key={movieReq.id} {...movieReq}/>) }
-        </div>
+      <div>
+        {movies.length > 0 ? (
+          <div className='container'>
+            <div className='grid'>
+              { movies.map((movieReq)=>
+              <MovieBox key={movieReq.id} {...movieReq}/>) }
+            </div>
+          </div>
+        ):(
+          <h2>Sorry Movie Not Found</h2>
+        )}
       </div>
     </>
   );
