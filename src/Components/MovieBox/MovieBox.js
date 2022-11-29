c8a5bb996f10476645e6c5cb92da784d2c2fc7f2
Satrya Tangguh <satryatp@gmail.com>
2022-11-29 18:08:21 +0700

    Modals styling

--- src/Components/MovieBox/MovieBox.js
+++ src/Components/MovieBox/MovieBox.js
@@ -1,9 +1,9 @@
-import { Modal, show, Button} from 'react-bootstrap';
+import { Modal, Button} from 'react-bootstrap';
 import React, {useState} from 'react';
 import './MovieBox.css';
 
 const API_IMG="https://image.tmdb.org/t/p/w500/";
-const MovieBox = ({title, poster_path, id, popularity, overview}) => {
+const MovieBox = ({title, poster_path, id, popularity, overview, backdrop_path}) => {
 
     const [show, setShow]=useState(false);
     const handleShow=()=>setShow(true);
@@ -13,39 +13,37 @@ const MovieBox = ({title, poster_path, id, popularity, overview}) => {
         <>
             <div className="card text-center my-3 bg-dark rounded-3">
                 <div className="card-body">
-                    <img className="card-img-top rounded-3" src={API_IMG + poster_path} />
+                    <img
+                        className="card-img-top rounded-3"
+                        src={API_IMG + poster_path}
+                        alt={title}
+                    />
                     <div className="card-body">
                         <button
                             type="button"
                             className="btn btn-dark"
                             onClick={handleShow}
                         >
-                            View More
+                            View Detail
                         </button>
-                        <Modal show={show} onHide={handleClose}>
+                        <Modal size="md" variant="dark" show={show} onHide={handleClose}>
                             <Modal.Header closeButton>
-                                <Modal.Title></Modal.Title>
+                                <Modal.Title />
                             </Modal.Header>
                             <Modal.Body>
                                 <img
-                                    className="card-img-top"
-                                    style={{ width: "14rem" }}
-                                    src={API_IMG + poster_path}
+                                    className="card-img-top rounded-2"
+                                    style={{ width: "100%" }}
+                                    src={API_IMG + backdrop_path}
                                     alt={title}
                                 />
-                                <h3>{title}</h3>
-                                <h4>Popularity: {popularity}</h4>
-                                <h5>Movie ID: {id}</h5>
-                                <br />
-                                <h6>Overview</h6>
-                                <p>{overview}</p>
+                                <h1 className="card-title my-2">{title}</h1>
+                                <h3 className="card-subtitle my-2">ID: {id} <span>Popularity: {popularity}</span></h3>
+                                <p className="card-overview mt-4">{overview}</p>
                             </Modal.Body>
                             <Modal.Footer>
-                                <Button
-                                    variant="secondary"
-                                    onClick={handleClose}
-                                >
-                                    Close
+                                <Button style={{ backgroundColor: '#FFD700', color: '#000', border: '1px solid #FFD700', fontSize: '15px', fontWeight: '600'}}>
+                                    Add to watchlist
                                 </Button>
                             </Modal.Footer>
                         </Modal>
