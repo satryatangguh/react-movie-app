433e4fb351d684b38fd82c86d423bcadaf382c85
Satrya Tangguh <satryatp@gmail.com>
2022-12-07 16:43:16 +0700

    Update Image in movie box

--- src/Components/MovieBox/MovieBox.js
+++ src/Components/MovieBox/MovieBox.js
@@ -3,7 +3,6 @@ import React, {useState} from 'react';
 import './MovieBox.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const API_IMG= process.env.REACT_APP_APIIMG;
 const MovieBox = ({title, name, poster_path, id, popularity, overview, backdrop_path}) => {
 
     const [show, setShow]=useState(false);
@@ -13,7 +12,7 @@ const MovieBox = ({title, name, poster_path, id, popularity, overview, backdrop_
     return (
         <>
             <div className="card h-100 text-center bg-dark rounded-3">
-                <img className="card-img-top h-100" src={API_IMG + poster_path} alt={title || name} />
+                <img className="card-img-top h-100" src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={title || name} />
                 <div className="card-body text-light">
                     <button type="button" className="btn btn-dark" onClick={handleShow}>
                         More & Detail
@@ -23,7 +22,7 @@ const MovieBox = ({title, name, poster_path, id, popularity, overview, backdrop_
                             <button type="button" class="btn-close" data-bs-dismiss="modal" onClick={handleClose} aria-label="Close"></button>
                         </div>
                         <div className='modal-body'>
-                            <img className="card-img-top rounded-2" style={{ height: "100%" }} src={API_IMG + backdrop_path} alt={title || name} />
+                            <img className="card-img-top rounded-2" style={{ height: "100%" }} src={`https://image.tmdb.org/t/p/w500/${backdrop_path}`} alt={title || name} />
                             <h1 className="card-title my-2">{title || name}</h1>
                             <h3 className="card-subtitle my-2">ID: {id} <span>Popularity: {popularity}</span></h3>
                             <p className="card-overview mt-4">{overview}</p>
