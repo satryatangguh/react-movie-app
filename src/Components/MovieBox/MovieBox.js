476059c6439afd7cbf8601cd51438780888e1234
Satrya Tangguh <satryatp@gmail.com>
2022-12-07 21:05:18 +0700

    Update MovieBox.js

--- src/Components/MovieBox/MovieBox.js
+++ src/Components/MovieBox/MovieBox.js
@@ -27,14 +27,14 @@ const MovieBox = ({title, name, poster_path, id, popularity, overview, backdrop_
                             <h2 className='fs-6 fw-semibold mt-3'>Overview</h2>
                             <p className="card-overview mt-0">{overview}</p>
                             <div className='row'>
-                                <div className='col-3 text-center'>
+                                <div className='col-6 text-center'>
                                     <p className="fs-6 fw-semibold my-2">Movie ID</p>
                                     <div className='d-inline-flex align-items-center'>
                                         <i className="ri-hashtag fs-5 me-2 card-logo bg-dark d-flex align-items-center justify-content-center"></i>
                                         <p className="card-score my-2">{id}</p>
                                     </div>
                                 </div>
-                                <div className='col-3 text-center'>
+                                <div className='col-6 text-center'>
                                     <p className="fs-6 fw-semibold my-2">Popularity</p>
                                     <div className='d-inline-flex align-items-center'>
                                         <i className="ri-star-fill fs-5 me-2 card-logo bg-dark d-flex align-items-center justify-content-center"></i>
